8b97be87263d9e90e9365f2e80eb33176b92b832
MrNikifor <osia.nikifor@gmail.com>
2022-10-31 22:34:02 +0300

    commit

---
+++ src/BasicsOfJava/Lesson2/Lesson2.java
@@ -0,0 +1,22 @@
+package BasicsOfJava.Lesson2;
+
+public class Lesson2 {
+    public static void main(String[] args) {
+        byte b10 = 12;
+        byte b2 = 0b110;
+        byte b8 = 014;
+        byte b16 = 0xC;
+
+        System.out.println(b16);
+
+        short sh10 = -1300;
+//        short sh2 = 0b1111111111111111111111111111111111111111111111111111101011101100;
+//        short sh8 = 01777777777777777775354;
+//        short sh16 = 0xFFFFFFFFFFFFFAEC;
+
+        System.out.println(sh10);
+
+
+
+    }
+}
